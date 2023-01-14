6c38358bd50433e67fe67d6a6c7c433ad8163702
Evgenyi <vrn.ocenka@gmail.com>
2023-01-14 14:43:50 +0300

    fix

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -4,25 +4,49 @@ using UnityEngine;
 
 public class TankController : MonoBehaviour
 {
-    public float moveSpeed;
-    
-    Rigidbody2D rigidbody2d;
-    // Start is called before the first frame update
+    public int speedRotation =100;
+    public Vector2 move;
+    public int speed = 2;
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        
-        Vector2 position = rigidbody2d.position;      
-        position.x = position.x + moveSpeed * horizontal * Time.deltaTime;
-        position.y = position.y + moveSpeed * vertical * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(0,0, speedRotation*Time.deltaTime);
 
-        rigidbody2d.MovePosition(position);
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(0, 0, -speedRotation * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector2.up * Time.deltaTime);
     }
+    
+    // public float moveSpeed;
+    //
+    // Rigidbody2D rigidbody2d;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     rigidbody2d = GetComponent<Rigidbody2D>();
+    // }
+    //
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     float horizontal = Input.GetAxis("Horizontal");
+    //     float vertical = Input.GetAxis("Vertical");
+    //     
+    //     Vector2 position = rigidbody2d.position;      
+    //     position.x = position.x + moveSpeed * horizontal * Time.deltaTime;
+    //     position.y = position.y + moveSpeed * vertical * Time.deltaTime;
+    //
+    //     rigidbody2d.MovePosition(position);
+    // }
 }
