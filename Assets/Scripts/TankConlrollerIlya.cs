6cf1516c168781fd0daf86b67f2bc597138418e3
BlackValet <tatar9927@gmail.com>
2023-01-14 20:13:03 +0300

    TankMove

---
+++ Assets/Scripts/TankConlrollerIlya.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankConlrollerIlya : MonoBehaviour
+{
+
+    public Rigidbody2D rd2d;
+    private Vector2 movementVector;
+    public float maxSpeed = 10;
+    public float rotationSpeed = 100;
+    public float turretRotationSpeed = 150;
+
+    public Transform turretParent;
+
+    private void Awake()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void HandleShoot()
+    {
+        Debug.Log("Shooting");
+    }
+
+
+    public void HandleMoveBody(Vector2 movementVector)
+    {
+        this.movementVector = movementVector;
+    }
+
+    public void HandleTurretMovement(Vector2 pointerPosition)
+    {
+        var turretDirection = (Vector3)pointerPosition - turretParent.position;
+
+        var desiredAngle = Mathf.Atan2(turretDirection.y, turretDirection.x) * Mathf.Rad2Deg;
+
+        var rotatrionStep = turretRotationSpeed * Time.deltaTime;
+        turretParent.rotation = Quaternion.RotateTowards(turretParent.rotation, Quaternion.Euler(0, 0, desiredAngle-90), rotatrionStep);
+    }
+
+    private void FixedUpdate()
+    {
+        rd2d.velocity = (Vector2)transform.up * movementVector.y * maxSpeed * Time.fixedDeltaTime;
+        rd2d.MoveRotation(transform.rotation * Quaternion.Euler(0, 0, -movementVector.x * rotationSpeed * Time.fixedDeltaTime));
+    }
+}
