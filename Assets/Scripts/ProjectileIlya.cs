2ba544740f264c6c4957e76fe05a1effe1ee671b
BlackValet <tatar9927@gmail.com>
2023-01-23 23:30:22 +0300

    shoot/hp

---
+++ Assets/Scripts/ProjectileIlya.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileIlya : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    // public Transform bulletStartPos;
+    public float bulletDamage = 5f;
+    
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        TankConlrollerIlya tankControl = other.gameObject.GetComponent<TankConlrollerIlya>();
+        if (tankControl)
+        {
+            tankControl.HpPlayerManager=bulletDamage;
+        }
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+       // float dist = Vector2.Distance(transform.position, bulletStartPos.position);
+       // Debug.Log(dist);
+       // if (dist>20)
+       // {
+       //     Destroy(gameObject);
+       // }
+    }
+}
--- Assets/Scripts/TankConlrollerIlya.cs
+++ Assets/Scripts/TankConlrollerIlya.cs
@@ -10,18 +10,54 @@ public class TankConlrollerIlya : MonoBehaviour
     public float maxSpeed = 10; //скорость
     public float rotationSpeed = 100; //скорость вращения танка
     public float turretRotationSpeed = 150; //скорость вращения турели
+    public float speedBullet;
 
     public Transform turretParent;
+    public Transform firePoint;
 
+    public GameObject projectilePrefab;
+
+    private float _currentHp=100;
+   
+
+    public float HpPlayerManager
+    {
+        get { return _currentHp; }
+
+        set { _currentHp -=value;
+            Debug.Log("damage=" + value);
+
+            if (_currentHp<0)
+            {
+                Destroy(gameObject);
+            }
+
+        } 
+    }
+
+   
     private void Awake()
     {
         rd2d = GetComponent<Rigidbody2D>();
     }
 
+   
+    private float SetHpPlayer (float hp)
+    {
+        return hp;
+    }
+
     public void HandleShoot()
     {
+        GameObject projectileOdject = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        var bullet = projectileOdject.GetComponent<Rigidbody2D>();
+     //   var bulletStart = projectileOdject.GetComponent<ProjectileIlya>();
+     //   bulletStart.bulletStartPos = this.transform;
+        bullet.AddForce(firePoint.up * speedBullet, ForceMode2D.Impulse);
         Debug.Log("Shooting");
+        
     }
+    
 
     public void HandleMoveBody(Vector2 movementVector)
     {
@@ -43,4 +79,5 @@ public class TankConlrollerIlya : MonoBehaviour
         rd2d.velocity = (Vector2)transform.up * movementVector.y * maxSpeed * Time.fixedDeltaTime;
         rd2d.MoveRotation(transform.rotation * Quaternion.Euler(0, 0, -movementVector.x * rotationSpeed * Time.fixedDeltaTime));
     }
+
 }
