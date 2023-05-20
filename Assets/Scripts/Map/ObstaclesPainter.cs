4811e6b7397fca15732d079d7657062b646b6874
Crazy_Master <gadaychenkov99@mail.ru>
2023-05-20 12:28:33 +0300

    fix

--- Assets/Scripts/Map/ObstaclesPainter.cs
+++ Assets/Scripts/Map/ObstaclesPainter.cs
@@ -5,6 +5,7 @@ using Random = UnityEngine.Random;
 
 public class ObstaclesPainter : MonoBehaviour
 {
+    private Transform _spawnedObjectsParent;
     [Serializable] public struct Obstacles
     {
         public string name;
@@ -12,9 +13,15 @@ public class ObstaclesPainter : MonoBehaviour
         public GameObject gameObject;
     }
     [SerializeField] public List<Obstacles> terrainLevel = new List<Obstacles>();
-    
+
+    private void Start()
+    {
+        
+    }
+
     public void Paint(float[,] terrainMap, float[,] obstacles1Map, float[,] obstacles2Map)
     {
+        CreateObjectParentIfNeeded();
         for (int i = 0; i < terrainMap.GetLength(0); i++)
         {
             for (int j = 0; j < terrainMap.GetLength(1); j++)
@@ -35,10 +42,22 @@ public class ObstaclesPainter : MonoBehaviour
                     if (objectMap != null)
                     {
                         
-                        var gameObject = Instantiate(objectMap, new Vector3(i + Random.Range(0,1), j + Random.Range(0,1)), Quaternion.Euler(0,0,Random.Range(0, 360)));
+                        var spawnedObject = Instantiate(objectMap, new Vector3(i + Random.Range(0,1), j + Random.Range(0,1)), Quaternion.Euler(0,0,Random.Range(0, 360)));
+                        spawnedObject.transform.SetParent(_spawnedObjectsParent);
                     }
                 }
             }
+        }
+    }
+
+    private void CreateObjectParentIfNeeded()
+    {
+        if (_spawnedObjectsParent == null)
+        {
+            string name = "Obstacles";
+            var parentObject = GameObject.Find(name);
+            if (parentObject != null) _spawnedObjectsParent = parentObject.transform;
+            else _spawnedObjectsParent = new GameObject(name).transform;
         }
     }
 }
